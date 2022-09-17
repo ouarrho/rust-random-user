f3eb952528b5956c12511bdd36396ae54a838dc4
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2022-09-17 12:42:53 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 
-	use ferris_says::say;
-	use std::io::{stdout, BufWriter};
-
 	fn main() {
 
-		let stdout  = stdout();
-		let message = String::from( "God Bless All The Rustaceans :)" );
-		let width   = message.chars().count();
-
-		let mut writer = BufWriter::new( stdout.lock() );
-
-		say( message.as_bytes(), width, &mut writer ).unwrap();
+		println!( "God Bless All The Rustaceans :)" );
 
 	}
